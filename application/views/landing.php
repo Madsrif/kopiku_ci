f3b792fc8b24c2832bf17553b68a0d47c9b16dc1
Madsrif <madsrif@gmail.com>
2022-09-08 15:00:22 +0700

    update v1

--- application/views/landing.php
+++ application/views/landing.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Ci</title>
+    <title>Bootstrap demo</title>
     <link href="<?php echo base_url()?>resource/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
