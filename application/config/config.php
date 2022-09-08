441d74170a0819b99e3f030904cee297808d77fa
Madsrif <madsrif@gmail.com>
2022-09-08 14:45:39 +0700

    Update base.url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kopiku_ci/';
+$config['base_url'] = 'https://kopiku-empat.sawala.dev/';
 
 /*
 |--------------------------------------------------------------------------
